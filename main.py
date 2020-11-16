868aa85264ca5fcaf17267e55504551cd3053f7e
TheGuy566 <54692320+TheGuy566@users.noreply.github.com>
2020-11-16 18:15:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-# To Do
-# get user input
-# calculate
-# output values
+# To Do:
+# - Probably try without using eval()
 
-
-values = input("Please enter two numbers : ")
-
-while isinstance(values, str):
-    try:
-        values = [int(i) for i in values.split()]
-    except ValueError:
-        values = input("Input should contain numbers only, please try again : ")
-
-print("""
-1. Add
-2. Subtract
-3. Multiply
-4. Divide
-""")
-operation = input("Please choose an operation : ")
-
-while isinstance(operation, str):
-    try:
-        operation = int(operation)
-    except ValueError:
-        operation = input("Input should contain numbers only, please try again : ")
-
-# can be simplified with dictionary
-if operation == 1:
-    result = values[0] + values[1]
-    print(result)
-else:
-    print("No valid operation chosen.")
+while True:
+    expression = input("Please enter an expression : ")
+    sanitized =  "".join([x for x in expression if x in "123456789-./*() "])
+    if sanitized == expression:
+        print (eval(expression))
+        break
+    else:
+        print ("Please enter a valid expression.")
